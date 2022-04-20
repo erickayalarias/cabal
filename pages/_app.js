1fbb08d575d3d4a71d93f536fc3553192a7f0079
Erick Ayala <erickayalrias@gmail.com>
2022-04-20 16:03:21 +0200

    maincabal

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import "regenerator-runtime/runtime"
+ThirdwebWeb3Provider
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import { Provider } from "react-redux";
+import { store, wrapper } from '../redux/store';
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const supportedChainsIds = [1]
+  const connectors = {
+    injected:{}
+  }
+  return (
+    <ThirdwebProvider
+      supportedChainsIds={supportedChainsIds}
+      connectors={connectors}
+    >
+    <Provider store={store}>
+    <Component {...pageProps} />
+    </Provider>
+    </ThirdwebProvider>
+  
+  )
 }
 
 export default MyApp
---
+++ pages/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexHome = () => {
+  return (
+    <div>IndexHome</div>
+  )
+}
+
+export default IndexHome
